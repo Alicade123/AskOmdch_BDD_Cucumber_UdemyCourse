33dd6fd065fdc773db7f83fc984d78fed35ca60f
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2026-01-13 21:42:21 +0200

    creating the step definition for demo purpose

--- src/test/java/step/definitions/Dummy.java
+++ src/test/java/step/definitions/Dummy.java
@@ -1,3 +1,34 @@
-public class Dummy{
+package step.definitions;
 
-} 
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.*;
+
+public class Dummy{
+    @Given ("I am dummy")
+    public void d(){
+
+    }
+    @When("I do dummy things")
+    public void du(){
+
+    }
+    @Then("dummy things happens")
+    public void dum(){
+
+    }
+
+    @Given("I am dummy account balance is ${int}")
+    public void openingBalance(int initial) {
+
+    }
+
+    @When("I withdraw ${int}")
+    public void withdrawBalance(int charge) {
+
+    }
+
+    @Then("the account balance should be ${int}")
+    public void closingBalance(int remaining) {
+
+    }
+}
