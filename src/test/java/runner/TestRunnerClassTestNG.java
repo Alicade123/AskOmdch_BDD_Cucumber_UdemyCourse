3b22ef27b53ca3b58059d27274ab64f4ecfaad35
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2026-01-13 21:41:56 +0200

    Adding the runner class using TestNG

--- src/test/java/runner/TestRunnerClassTestNG.java
+++ src/test/java/runner/TestRunnerClassTestNG.java
@@ -1,14 +1,23 @@
-package runner;
-
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/resources/features",
-        glue = "step.definitions"
-)
-public class TestRunnerClass {
-
-}
+//package runner;
+//
+//
+//import io.cucumber.testng.AbstractTestNGCucumberTests;
+//import io.cucumber.testng.CucumberOptions;
+//import org.junit.AfterClass;
+//import org.junit.BeforeClass;
+//
+//@CucumberOptions(
+//        features = "src/test/resources/features",a
+//        glue = "step.definitions"
+//)
+//public class TestRunnerClassTestNG extends AbstractTestNGCucumberTests {
+//
+//        @BeforeClass
+//        public static void setUp(){
+//            System.out.println("This is before class annotation");
+//        }
+//        @AfterClass
+//        public static void tearDown(){
+//            System.out.println("This is after class annotation");
+//
+//}
