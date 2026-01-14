36186012be52e2ce0ef0f046dea1aa19f9550e58
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2026-01-14 20:49:23 +0200

    Update AddToCart step definitions

--- src/test/java/step/definitions/AddToCartSD.java
+++ src/test/java/step/definitions/AddToCartSD.java
@@ -1,16 +1,23 @@
 package step.definitions;
 
+import io.cucumber.java.ParameterType;
 import io.cucumber.java.en.*;
+import object.Product;
 
 public class AddToCartSD {
+
     @Given("I'm on the Store Page")
     public void goToStorePage() {
         System.out.println("Here we go");
     }
-    @When("I add a {string} to the Cart")
-    public void addProductToCart(String string) {
+    @When("I add a {product} to the Cart")
+    public void addProductToCart(Product product) {
+//        Product product = new Product(productName);
+        System.out.println("printlnThe product name is: "+product.getProductName());
     }
-    @Then("I see {int} {string} in the Cart")
-    public void checkNumberOfProductAdded(Integer int1, String string) {
+    @Then("I see {int} {product} in the Cart")
+    public void checkNumberOfProductAdded(Integer int1, Product product) {
+        System.out.println("printlnThe product name is: "+product.getProductName());
+
     }
 }
